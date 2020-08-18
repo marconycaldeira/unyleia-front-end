cd7123dc17cc2848302c033ba8ab7ad6bc2b5249
Marcony Caldeira <marcony@MacBook-Air-de-Marcony.local>
2020-08-18 17:21:33 -0300

    Adiciona e configura pacotes de UI e SEO

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,35 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  pathPrefix: "/gatsby-react-bootstrap-starter",
+  siteMetadata: {
+    title: `Gatsby React Bootstrap Starter`,
+    description: `A starter that includes react-bootstrap and react-icons, along with SASS compilation.`,
+    author: `Billy Jacoby`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-plugin-sass`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-react-bootstrap`,
+        short_name: `react-bootstrap`,
+        start_url: `/`,
+        background_color: `#20232a`,
+        theme_color: `#20232a`,
+        display: `minimal-ui`,
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
 }
